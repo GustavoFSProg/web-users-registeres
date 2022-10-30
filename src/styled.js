a8a1da93cf20746b5060f3cfc23a699941a1a13b
Gustavo <gustavo.prog40@gmail.com>
2022-10-30 16:23:58 -0300

    color siainsG

--- src/styled.js
+++ src/styled.js
@@ -106,7 +106,7 @@ export const TitleRegister = styled.h1`
   @media screen and (max-width: 700px) {
     font-size: 1.5rem;
     margin-bottom: 90px;
-    margin-top: -160px;
+    margin-top: -50px;
   }
 `
 
